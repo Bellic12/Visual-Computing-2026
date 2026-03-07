e9c5b0632a63fe45d056d4517b348ae20e97e8f1
Felipe <jff.garzon2006@gmail.com>
2026-03-07 17:52:55 -0500

    3.4 ThreeJs: Add Advanced effects in shaders

---
+++ Semana_03_Pipeline_Grafico/semana_03_4_etapas_pipeline_programable/README.md
@@ -0,0 +1,58 @@
+# Taller - Proyecciones 3D: Cómo ve una Cámara Virtual
+## Integrantes
+- Juan David Buitrago Salazar
+- Juan David Cardenas Galvis
+- Nicolás Rodríguez Piraban
+- Camilo Andres Medina Sanchez
+- Juan Felipe Fajardo Garzón
+
+**Fecha de entrega:**  09/03/2026
+
+## Descripción breve: 
+
+
+
+## Implementaciones: 
+
+### Unity:
+
+### Three.js
+
+
+## Resultados visuales:
+
+
+### Unity:
+
+
+### Three.js
+Esfera con shaders proporcionados
+![sphere_shaders](media/sphere_shaders.gif)
+
+Ruido Procedural
+![procedural noise](media/sphere_procedural_noise.gif)
+
+Animación con uniform
+![uniform Animation](media/sphere_uniforms_animation.gif)
+
+Efecto Fresnel
+![fresnel effect](media/sphere_fresnel_effect.gif)
+
+Varios efectos juntos (Ruido procedural, Efecto fresnel, viñetado y rim lightning)
+
+![all efects](media/sphere_all_efects.gif)
+
+
+## Código relevante: 
+
+
+### Unity
+
+### Three.js
+
+
+## Aprendizajes y dificultades: 
+
+
+## Contribuciones del grupo
+
--- Semana_03_Pipeline_Grafico/semana_03_4_etapas_pipeline_programable/threejs/shader_3_4/src/main.js
+++ Semana_03_Pipeline_Grafico/semana_03_4_etapas_pipeline_programable/threejs/shader_3_4/src/main.js
@@ -9,29 +9,71 @@ import * as THREE from 'three';
 
         //Código proporcionado para los shaders
         const vertexShader = `
-            uniform float time;
             varying vec2 vUv;
+            varying vec3 vNormal;
+            uniform float time;
+
+            // Funcion para generar el rudio (semi-aleatoria)
+            float hash(float n) { return fract(sin(n) * 43758.5453123); }
+            float noise(vec3 x) {
+                vec3 p = floor(x);
+                vec3 f = fract(x);
+                f = f*f*(3.0-2.0*f);
+                float n = p.x + p.y*57.0 + 113.0*p.z;
+                return mix(mix(mix(hash(n+0.0), hash(n+1.0),f.x),
+                        mix(hash(n+57.0), hash(n+58.0),f.x),f.y),
+                        mix(mix(hash(n+113.0),hash(n+114.0),f.x),
+                        mix(hash(n+170.0),hash(n+171.0),f.x),f.y),f.z);
+            }
             void main() {
                 vUv = uv;
-                vec3 pos = position;
-                // Deformación de onda
-                pos.z += sin(pos.x * 5.0 + time) * 0.2;
+                vNormal = normal;
+
+                // Procedural Noise
+                float noiseVal = noise(vec3(position.x * 2.0, position.y * 2.0, time));
+                vec3 pos = position + normal * noiseVal * 0.4;
+
+                // Animacion con uniforms
+                float distortion = sin(position.y * 3.0 + time) * 0.2;
+                pos = pos + normal * distortion;
+
+
+                // Deformación con onda (original)
+                // vec3 pos = position;
+                // pos.z += sin(pos.x * 5.0 + time) * 0.1;
+
                 gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
             }
         `;
 
         const fragmentShader = `
             varying vec2 vUv;
+            varying vec3 vNormal;
             uniform float time;
             void main() {
-                // Color basado en coordenadas y tiempo
-                vec3 color = vec3(vUv.x, vUv.y, abs(sin(time)));
-                gl_FragColor = vec4(color, 1.0);
+
+                // Fresnel Effect
+                vec3 viewDir = vec3(0.0, 0.0, 1.0);
+                float fresnel = pow(1.0 - dot(vNormal, viewDir), 2.0);
+
+                // Rim Lighting
+                vec3 baseColor = vec3(vUv.x, 0.5, vUv.y);
+                vec3 color = mix(baseColor, vec3(1.0), fresnel);
+                // Viñetado
+                float v = smoothstep(0.7, 0.2, distance(vUv, vec2(0.5)));
+
+                // Shader de color Original
+                // vec3 color = vec3(vUv, 0.5);
+                // color *= dot(vNormal, vec3(0, 0, 1)) * 0.5 + 0.5;
+
+
+                gl_FragColor = vec4(color*v, 1.0);
             }
+
         `;
 
         //Creación del objeto con el shader
-        const geometry = new THREE.PlaneGeometry(5, 5, 32, 32); // Plano con muchos segmentos para que se doble
+        const geometry = new THREE.SphereGeometry(2, 64, 64); // Plano con muchos segmentos para que se doble
         const material = new THREE.ShaderMaterial({
             uniforms: {
                 time: { value: 0.0 }
@@ -49,8 +91,13 @@ import * as THREE from 'three';
         //Animación principal
         function animate(now) {
             requestAnimationFrame(animate);
-            material.uniforms.time.value = now * 0.001;
-            
+            const t = now * 0.001;
+            material.uniforms.time.value = t;
+
+            //Rotación automática del objeto
+            //mesh.rotation.y = t * 0.5;
+            //mesh.rotation.x = t * 0.2;
+
             renderer.render(scene, camera);
         }
         animate();
